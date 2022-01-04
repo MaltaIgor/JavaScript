5805ac27e480f5e60302bc1502ad4747ae6d0f71
Igor Malta <igorbrunomalta@gmail.com>
2022-01-04 20:35:26 -0300

    Create retira_duplicados.js

---
+++ Training/Aula11/retira_duplicados.js
@@ -0,0 +1,18 @@
+
+var head = [-2,-2,-1,-1,-1,1, 1, 2, 2, 2, 2 ,3,3,3,3,3,4,4,4,4,4,5,5,5,6,7,8,9,9,9,9,9,9,45,45,56,56,86,86,89,96];
+//Number(head)
+//head.sort()
+//console.log(head)
+lista=[]
+for (let i=0; i<=head.length; i++)  {
+    if (i==0){
+        x = head[i]
+        lista.push(x)
+    } else if (x < head[i]){
+        x = head[i]
+        lista.push(x)
+    }
+   
+}
+head=lista
+console.log(head);
