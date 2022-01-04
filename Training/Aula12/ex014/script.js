ec95a651d5ec0cbae7516601c066c0bf8879a2f0
Igor Malta <igorbrunomalta@gmail.com>
2022-01-04 10:06:13 -0300

    Updating

--- Training/Aula12/ex014/script.js
+++ Training/Aula12/ex014/script.js
@@ -7,17 +7,17 @@ function carregar(){
     msg.innerHTML = `Agora são ${hora} horas e ${minuto} minutos.`
     if (hora>=0 && hora <12){
         //BOM DIA!!
-        msg.innerHTML += '<P>Bom dia!!</P>'
+        msg.innerHTML += '<P><strong>Bom dia!!!</strong></P>'
         img.src = 'manha.png'
         document.body.style.background = '#ffeaa8'
     } else if (hora>=12 && hora<18){
         //BOA TARDE!
-        msg.innerHTML += '<P>Boa tarde!!</P>'
+        msg.innerHTML += '<P><strong>Boa tarde!!!</strong></P>'
         img.src = 'tarde.png'
         document.body.style.background = '#e4730b'
     } else {
         //BOA NOITE!
-        msg.innerHTML += '<P>Boa noite!!</P>'
+        msg.innerHTML += '<P><strong>Boa noite!!!</strong></P>'
         img.src = 'noite.png'
         document.body.style.background = '#14161a'
     }
