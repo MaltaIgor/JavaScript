e669b4d139d455871e4d8db01f594ad66f9be5b0
Igor Malta <igorbrunomalta@gmail.com>
2022-01-05 17:19:40 -0300

    Update script.js

--- Training/Aula12/ex014/script.js
+++ Training/Aula12/ex014/script.js
@@ -3,7 +3,7 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     //var hora = data.getHours()
-    var hora = 18
+    var hora = 14
     var minuto = data.getMinutes()
     msg.innerHTML = `Agora são ${hora} horas e ${minuto} minutos.`
     if (hora>=0 && hora <12){
