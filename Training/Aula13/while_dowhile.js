6cc6d2e082fe47465a45d68c4ad9d98b5920615d
Igor Malta <igorbrunomalta@gmail.com>
2022-01-05 21:22:21 -0300

    Create while_dowhile.js

---
+++ Training/Aula13/while_dowhile.js
@@ -0,0 +1,18 @@
+var c=1
+
+while (c<=6){
+console.log('tudo bem?')
+c++
+}
+
+console.log('--------------')
+
+do {
+ console.log('tudo bem?')
+ c--
+}while (c>=1)
+
+
+console.log('--------------')
+
+var c=
