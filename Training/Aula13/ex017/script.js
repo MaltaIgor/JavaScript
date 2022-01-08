a562ad0e8eaa507aa8c1d21f48f9c6335405e705
Igor Malta <igorbrunomalta@gmail.com>
2022-01-08 11:45:44 -0300

    Update

--- Training/Aula13/ex017/script.js
+++ Training/Aula13/ex017/script.js
@@ -1,1 +1,16 @@
-alert('Ola')
+function tabuada(){
+let n = Number.parseInt(window.document.getElementById('numero').value)
+let tab = window.document.querySelector('select#seltab')
+
+if(n.value.length==0){
+    window.alert('Digite um dumero!!!')
+} else {
+    tab.innerHTML = ""
+for(c=1;c<=10;c++){
+    let item = document.createElement('option')
+    item.text = `${n} + ${c} = ${n*c}` 
+    item.value = `tab${c}`
+    tab.appendChild(item)
+}
+}
+}
