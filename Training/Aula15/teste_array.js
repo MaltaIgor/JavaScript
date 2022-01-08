21034b816ccdfc6e1525fe806a27d768fc178e78
Igor Malta <igorbrunomalta@gmail.com>
2022-01-08 12:46:05 -0300

    Create teste_array.js

---
+++ Training/Aula15/teste_array.js
@@ -0,0 +1,16 @@
+let num = [1,2,3,4]
+console.log(num)
+console.log('-----------------')
+console.log(typeof num)
+num[6]=1
+console.log('-----------------')
+console.log(num)
+console.log('-----------------')
+for (let pos in num){
+    console.log(num[pos])
+}
+console.log('-----------------')
+console.log(num.indexOf(3))
+console.log('-----------------')
+nome = 'asd'
+console.log(nome[0])
