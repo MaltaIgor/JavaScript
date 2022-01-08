7673842a43a6edbdf8ae3f0e08d1a50667ccb3c7
Igor Malta <igorbrunomalta@gmail.com>
2022-01-08 13:20:59 -0300

    update

---
+++ Training/Aula15/teste_funcao.js
@@ -0,0 +1,13 @@
+function parimpar(n) {
+    if(n%2==0){
+        return "par"
+    }else {
+        return "impar"
+    }
+}
+
+console.log(parimpar(5))
+let v = function(n){
+    return n*2
+}
+console.log(v(5))
