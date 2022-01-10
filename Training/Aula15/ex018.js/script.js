fdee1004b0565d5b72101b28ba21064832289391
Igor Malta <igorbrunomalta@gmail.com>
2022-01-10 19:32:06 -0300

    update

--- Training/Aula15/ex018.js/script.js
+++ Training/Aula15/ex018.js/script.js
@@ -25,7 +25,33 @@ function adicionar(){
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        res.innerHTML = ''
     } else {
-        window,alert('Valor invélido ou já encontrado na lista.')
+        window,alert('Valor inválido ou já encontrado na lista.')
+
+    }
+    num.value = ''
+    num.focus()
+}
+function finalizar(){
+    if(valores.length == 0 ){
+        window.alert('Adicione valores antes de finalizar.')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores){
+            soma+= valores[pos]
+            if(valores[pos]>maior) maior = valores[pos]
+            if(valores[pos]<maior) menor = valores[pos]
+        } media = soma/tot
+        res.innerHTML = ``
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados. </p>`
+        res.innerHTML += `<p>O maior vaor informado foi ${maior}. </p>`
+        res.innerHTML += `<p>O menor vaor informado foi ${menor}. </p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}. </p>`
+        res.innerHTML += `<p>A média dos valores é ${media}. </p>`
     }
 }
